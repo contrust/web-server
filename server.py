4e5c5d6151f391e6144e9ebca7673ec5be7cdd14
contrust <2001raf@gmail.com>
2021-07-02 21:54:24 +0500

    added processPoolExecutor, get_all_client_data method has a timeout parameter

--- server.py
+++ server.py
@@ -1,6 +1,7 @@
 from config import Config
 from socket import socket, AF_INET, SOCK_STREAM
 import concurrent.futures
+from select import select
 import errno
 from request import Request
 
@@ -9,25 +10,29 @@ class Server:
     config = Config()
 
     def start(self):
-        server_socket = socket(AF_INET, SOCK_STREAM)
-        server_socket.bind((self.config.host, self.config.port))
-        server_socket.listen()
-        while 1:
-            client_socket, client_address = server_socket.accept()
-            with concurrent.futures.ThreadPoolExecutor() as executor:
-                future = executor.submit(self.get_all_client_data, client_socket)
-                return_value = future.result()
-                print(Request(return_value).body)
+        with concurrent.futures.ProcessPoolExecutor(max_workers=self.config.max_processes) as executor:
+            with socket(AF_INET, SOCK_STREAM) as server_socket:
+                server_socket.bind((self.config.host, self.config.port))
+                server_socket.listen()
+                while 1:
+                    client_socket, client_address = server_socket.accept()
+                    executor.submit(serve, client_socket)
 
-    @staticmethod
-    def get_all_client_data(client: socket):
-        client.setblocking(False)
-        total_data = []
-        while 1:
-            try:
-                data = client.recv(8192)
-                total_data.append(data)
-            except IOError as e:
-                if e.errno == errno.EWOULDBLOCK:
-                    break
-        return b''.join(total_data)
+
+def get_all_client_data(client: socket, timeout=5):
+    total_data = []
+    client.setblocking(False)
+    ready = select([client], [], [], timeout)
+    while ready[0]:
+        try:
+            data = client.recv(8192)
+            total_data.append(data)
+        except Exception as e:
+            if e.errno == errno.EWOULDBLOCK:
+                break
+    return b''.join(total_data)
+
+
+def serve(client: socket):
+    while data := get_all_client_data(client):
+        print(data)
