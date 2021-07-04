d2bd345356050e47b24f1246a080f93d362c1e2a
contrust <2001raf@gmail.com>
2021-07-04 17:02:47 +0500

    proxy_pass as dictionary in config where key is location and value is proxy server's absolute path

--- config.py
+++ config.py
@@ -1,8 +1,11 @@
 class Config:
     host: str = ''
     port: int = 8080
-    proxy_host: str = 'www.google.com'
+    proxy_host: str = 'googl.com'
     proxy_port: int = 80
     proxy_on: bool = True
     connection_timeout: float = 1
     max_threads: int = 5
+    proxy_pass: dict[str, str] = {
+        '/location/': 'http://www.google.com/suka'
+    }
