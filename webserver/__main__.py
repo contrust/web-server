8b4f90e493b6652906f5cd797285c648fc923c8b
contrust <2001raf@gmail.com>
2021-07-25 16:11:44 +0500

    added config doc

--- webserver/__main__.py
+++ webserver/__main__.py
@@ -11,12 +11,17 @@ def parse_arguments():
         prog=None if not globals().get('__spec__')
         else f'python3 -m {__spec__.name.partition(".")[0]}'
     )
-    parser.add_argument('-c', '--config', metavar='CONFIG_FILE', required=False,
-                        help='run server with config file containing '
-                             'dictionary with json format, '
-                             'config.py values are used by default '
-                             'if they are not mentioned in this file')
-    parser.add_argument('--get-config', metavar='OUTPUT_FILE', required=False,
+    parser.add_argument('-c', '--config',
+                        metavar='CONFIG_FILE',
+                        required=False,
+                        help='run server with config file settings')
+    parser.add_argument('--get-config',
+                        metavar='OUTPUT_FILE',
+                        required=False,
+                        help='get config file with default values and exit')
+    parser.add_argument('--config-doc',
+                        action='store_true',
+                        required=False,
                         help='get config file with default values and exit')
     return parser.parse_args()
 
@@ -25,6 +30,9 @@ def main():
     try:
         config = Config()
         args_dict = vars(parse_arguments())
+        if args_dict['config_doc']:
+            print(config.__doc__)
+            sys.exit()
         if args_dict['get_config']:
             config.unload(args_dict['get_config'])
             sys.exit()
